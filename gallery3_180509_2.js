69a5504152d772f4ab09aa36c5cf1e5e7e0e24f0
98sena <37644146+98sena@users.noreply.github.com>
2018-05-10 10:56:33 +0900

    Update gallery3_180509_2.js

--- gallery3_180509_2.js
+++ gallery3_180509_2.js
@@ -1,5 +1,5 @@
 var img1;
-var type;
+
 var size;
 
 function setup() {
@@ -9,21 +9,11 @@ function setup() {
   createCanvas(700,543);
   background(0);
   size=80;
-  type=0;
+  
 }
 
 function draw() {
-  if(type==0){
-    for(var k=0; k<500; k+=1) {
-      var x =random(width);
-      var y =random(height);
-      var c = img1.get(int(x),int(y));
-      fill(c,100);
-      noStroke();
-      ellipse(x,y,10,10);
-    }
-  }
-  else{
+
     for(var y=0;y<img1.height;y+=10){
     for(var x=0;x<img1.width;x+=10){
       var c = img1.get(x,y);
@@ -38,18 +28,10 @@ function draw() {
       }
     }
   }
-  }
+
 }
 
 
-function mousePressed(){
-  if(type ==0){
-    type=1;
-  }
-  else {
-    type=0;
-  }
-}
 
 function keyPressed(){
   if(keyCode===UP_ARROW){
